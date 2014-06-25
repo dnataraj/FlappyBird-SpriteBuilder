547601937e7927651e5818a7ad0fcdabb94f2626
Deepak Natarajan <dnataraj@trilobytesystems.com>
2014-06-25 23:09:58 +0300

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,11 +9,24 @@
     character = (Character *)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    // this will be run every frame.
+    // delta is the time that has elapsed since the last time it was run. This is usually 1/60, but can be bigger if the game slows down
+    
+    // Check to see if 2 seconds have passed
+    timeSinceObstacle += delta;
+    if (timeSinceObstacle > 2.0f) {
+        // add a new obstacle
+        [self addObstacle];
+        
+        // then reset the timer
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
